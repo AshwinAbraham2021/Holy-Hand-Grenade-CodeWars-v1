49012194dc785b67a265f9725c90793b9ef3e1dc
k-rt-k <94541845+k-rt-k@users.noreply.github.com>
2021-12-23 14:58:09 +0530

    minor

--- scriptblue.py
+++ scriptblue.py
@@ -10,9 +10,10 @@ def CoordStr(x,y): #assumes x,y<100
         ys=str(y)
     return xs+ys
 
+#def ()
 def ActRobot(robot):
 
-    ## if base is found, return as b + x_be +| y_be + .
+    ## if base is found, return as b + Coordstr .
         if robot.GetVirus() > 1000:
                 robot.DeployVirus(200)
         
@@ -29,24 +30,25 @@ def ActBase(base):
 
     #count of classes???
 
-    bel = base.GetElixir()
-    while bel > 600:
-        if bel > 1500:
+
+    if base.GetElixir()> 600:
+        while base.GetElixir() > 1500:
             base.create_robot('m')  # mixed capability ->depends
-        elif bel > 1000:
+        while base.GetElixir() > 1000:
             base.create_robot('a')  # attack capability ->more freedom of movement and attack
-        else:
+        while base.GetElixir() > 600:
             base.create_robot('d')  # defence capability ->more conservative with movement
 
     All=base.GetListOfSignals()
     for L in All:
-        if L[0]=='b':
+        if len(L)>0 and L[0]=='b':
             base.SetYourSignal(L[:5]+base.GetYourSignal()[5:]) #checkforerrors
 
-    enemies_near=(base.investigate_up()=='enemy')or(base.investigate_down()=='enemy')or(base.investigate_left()=='enemy')or(base.investigate_right()=='enemy')or(base.investigate_ne()=='enemy')or(base.investigate_nw()=='enemy')or(base.investigate_se()=='enemy')or(base.investigate_sw()=='enemy')
-    if enemies_near:
-        base.DeployVirus(500)
-        base.SetYourSignal(base.GetYourSignal()+'h')
+    def enemies_near():
+        return (base.investigate_up()=='enemy')+(base.investigate_down()=='enemy')+(base.investigate_left()=='enemy')+(base.investigate_right()=='enemy')+(base.investigate_ne()=='enemy')+(base.investigate_nw()=='enemy')+(base.investigate_se()=='enemy')+(base.investigate_sw()=='enemy')
+    if enemies_near():
+        base.DeployVirus(800)
+        base.SetYourSignal(base.GetYourSignal()+'h'+str(enemies_near))
 
     ##add the defense/attack mechn!!
     return
